ec972f875b1adc720d931029376e8351619ef11b
Shreya Khambe <113600207+Shreyak810@users.noreply.github.com>
2024-08-13 09:19:15 +0530

    Update Calculate_value_of_Pi.c

--- Assignment-2/Calculate_value_of_Pi.c
+++ Assignment-2/Calculate_value_of_Pi.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main() {
+    int numPoints = 1000000;  // Number of random points to generate
+    int count = 0;            // Points inside the quarter circle
+
+    // Parallel region for Monte Carlo simulation
+    #pragma omp parallel for reduction(+:count)
+    for (int i = 0; i < numPoints; i++) {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+        if (x * x + y * y <= 1.0) {
+            count++;
+        }
+    }
+
+    // Calculate Pi
+    double pi = 4.0 * (double)count / numPoints;
+  
+    // Print the results
+    printf("Estimated value of Pi: %f\n", pi);
+    return 0;
+}
 
