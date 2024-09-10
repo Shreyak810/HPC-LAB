adc648bdcc40b50ef4b2ec902f28f132ce983d4b
Shreya Khambe <113600207+Shreyak810@users.noreply.github.com>
2024-09-10 15:54:06 +0530

    Create Q1.c

---
+++ Assignment - 5/Q1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    // Array of family member names
+    const char* family_members[] = {"Shreya", "Alice", "Bob", "Charlie"};
+    int num_members = 4;
+
+    // Parallel region with a specified number of threads equal to family members
+    #pragma omp parallel num_threads(num_members)
+    {
+        int thread_id = omp_get_thread_num();  // Get the thread ID
+        if (thread_id < num_members) {
+            printf("Thread ID: %d, Name: %s\n", thread_id, family_members[thread_id]);
+        }
+    }
+
+    return 0;
+}
